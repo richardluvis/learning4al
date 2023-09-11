b3c5090e48a1cea80032a72d9363c92590b28b2f
richardluvis <lopezcard35@gmail.com>
2023-09-10 21:51:57 -0500

    Nada
    Ya logre redireccionar

--- server.js
+++ server.js
@@ -39,10 +39,14 @@ app.post('/registrar', (req, res) => {
     connection.query(query, values, (error, results) => {
         if (error) {
             console.error('Error al guardar el usuario:', error);
-            res.render('Error al registrar el usuario.');
+            const successMessage = encodeURIComponent('Error al guardar el usuario:');
+            const redirectURL = '/usuarios.html' + '?mensaje=' + successMessage;
+            res.redirect(redirectURL);
         } else {
             console.log('Usuario registrado exitosamente.');
-            res.send('help me i cat do it');
+            const successMessage = encodeURIComponent('Usuario registrado exitosamente.');
+            const redirectURL = '/sesion.html' + '?mensaje=' + successMessage;
+            res.redirect(redirectURL);
 
         }
     });
